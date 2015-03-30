1bedef271db67864e4e4ee7f7c142e9cb58008ed
Héctor Acosta <hacost@hotmail.com>
2015-03-30 00:17:27 -0600

    add jquery

--- js/app.js
+++ js/app.js
@@ -1,70 +1,80 @@
-const startGame = "../start_game.html"
-const resultGame = "../result_game.html"
+    $(document).ready(function() {
+    	var piedra = 0;
+		var papel = 1;
+		var tijera = 2;
+		var opciones = ["Piedra","Papel","Tijeras"];
+        var opcionUsuario;
+        $("#procesar").click(function(){
+		
+        var opcionMaquina = aleatorio(0,2);
+        var selected = $("input[type='radio'][name='userOption']:checked");
+		if (selected.length > 0) {
+		    opcionUsuario = selected.val();
+		}
 
-var redirect = function (url){
-	
-}
-function aleatorio(minimo, maximo)
-{
-   var numero = Math.floor( Math.random() * (maximo - minimo +1) +minimo);
-   return numero;
-}
-var piedra = 0;
-var papel = 1;
-var tijera = 2;
-var opciones = ["Piedra","Papel","Tijeras"];
+		function aleatorio(minimo, maximo)
+		{
+		   var numero = Math.floor( Math.random() * (maximo - minimo +1) +minimo);
+		   return numero;
+		}
 
-var opcionUsuario;
-var opcionMaquina = aleatorio(0,2);
+		console.log(opcionUsuario);
+		if(opcionUsuario == piedra)
+		{
+			if (opcionMaquina == piedra) 
+			{
+		            alert("Enpate!");
+			}
+			else if (opcionMaquina == papel) 
+			{
+		            alert("Perdiste :(");
+			}
+			else if (opcionMaquina == tijera) 
+			{
+		            alert("Ganaste!");
+			}
+		}
+		else if(opcionUsuario == papel)
+		{
+				if (opcionMaquina == piedra) 
+			{
+		            alert("Ganaste!");
+			}
+			else if (opcionMaquina == tijera) 
+			{
+		            alert("Perdiste:(");
+			}
+			else if (opcionMaquina == papel) 
+			{
+		            alert("Enpate!");
+			}
+		}
 
-opcionUsuario = prompt("Que eliges?\nPiedra: 0\nPapel: 1\nTijera: 2",0);
+		else if(opcionUsuario == tijera)
+		{
+				if (opcionMaquina == papel) 
+			{
+		            alert("Ganaste!");
+			}
+			else if (opcionMaquina == piedra) 
+			{
+		            alert("Perdiste :(");
+			}
+			else if (opcionMaquina == tijera) 
+			{
+		            alert("Enpate!");
+			}
+		} 
+        });
+
+    });
+
+
+
+
+// opcionUsuario = prompt("Que eliges?\nPiedra: 0\nPapel: 1\nTijera: 2",0);
      
-alert("Elegiste " + opciones[opcionUsuario])
-alert("Javasript eligio " + opciones[opcionMaquina])
+//alert("Elegiste " + opciones[opcionUsuario])
+//alert("Javasript eligio " + opciones[opcionMaquina])
 
-if(opcionUsuario == piedra)
-{
-	if (opcionMaquina == piedra) 
-	{
-            alert("Enpate!");
-	}
-	else if (opcionMaquina == papel) 
-	{
-            alert("Perdiste :(");
-	}
-	else if (opcionMaquina == tijera) 
-	{
-            alert("Ganaste!");
-	}
-}
-else if(opcionUsuario == papel)
-{
-		if (opcionMaquina == piedra) 
-	{
-            alert("Ganaste!");
-	}
-	else if (opcionMaquina == tijera) 
-	{
-            alert("Perdiste:(");
-	}
-	else if (opcionMaquina == papel) 
-	{
-            alert("Enpate!");
-	}
-}
 
-else if(opcionUsuario == tijera)
-{
-		if (opcionMaquina == papel) 
-	{
-            alert("Ganaste!");
-	}
-	else if (opcionMaquina == piedra) 
-	{
-            alert("Perdiste :(");
-	}
-	else if (opcionMaquina == tijera) 
-	{
-            alert("Enpate!");
-	}
-} 
